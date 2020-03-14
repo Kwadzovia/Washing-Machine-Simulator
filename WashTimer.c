963e7f743c810ec95aaf0510266ac3412207ea81
Selikem Kwadzovia <skwadzov@sfu.ca>
2020-03-14 13:58:08 -0700

    Wash Timer Framework

---
+++ WashTimer.c
@@ -0,0 +1,28 @@
+/*
+ * WashTimer.c
+ *
+ *  Created on: Mar 14, 2020
+ *      Author: selik
+ */
+// Counts down from 5 on 7-seg
+
+volatile extern unsigned long washCount0;
+
+void Wash_Timer(void){
+    //Pause timer
+    //Set counter to 5
+    //Reset timer
+    //Start timer
+
+    //Loop if counter != 0
+    //Display count on 7seg
+
+    //Display 0
+
+    //return
+
+
+}
+
+
+
---
+++ WashTimer.h
@@ -0,0 +1,15 @@
+/*
+ * WashTimer.h
+ *
+ *  Created on: Mar 14, 2020
+ *      Author: selik
+ */
+
+#ifndef WASHTIMER_H_
+#define WASHTIMER_H_
+
+void Wash_Timer(void);
+
+
+
+#endif /* WASHTIMER_H_ */
