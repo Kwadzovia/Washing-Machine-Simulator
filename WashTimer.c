516b94c01090aeb666224f839e79467eb4937e2b
Caleb <cgimpel@sfu.ca>
2020-04-10 12:44:15 -0700

    [CG] Remove comments

--- WashTimer.c
+++ WashTimer.c
@@ -13,9 +13,6 @@
 volatile extern unsigned long washCount;
 volatile extern unsigned long systickCount;
 
-// TODO: Turn this into a timer. Rather than just starting at some number and counting down,
-// we could make this like a little spinner. It starts at the bottom  and goes around cw until it completes the circle.
-// This would make a better representation of a timer. Not sure it's possible to do with the 74LS47 chip.
 void WashTimer(unsigned long startCount){
     // Initialize count and timer
     washCount = startCount;
